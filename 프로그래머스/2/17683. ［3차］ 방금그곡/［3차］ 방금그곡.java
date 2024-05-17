1ea540348d02f7fd957e60ea19a2dc268e4beb29
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-05-17 21:44:01 +0900

    Update ［3차］ 방금그곡.java

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String m, String[] musicinfos) throws Exception {
+    public String solution(String m, String[] musicinfos) {
         String answer = "";
         ArrayList<Music> foundMusicList = new ArrayList<>();
         m = replace(m);
         for(String musicinfo : musicinfos){
             String[] infos = musicinfo.split(",");
             Music music = new Music(infos[0], infos[1], infos[2], replace(infos[3]), m);
-            //music.check();
-            if(music.isfindCode(m)) foundMusicList.add(music);
+            if(music.code.contains(m)) foundMusicList.add(music);
         }
         Collections.sort(foundMusicList);
         if(foundMusicList.size() == 0) answer = "(None)";
@@ -27,34 +26,24 @@ class Solution {
         target = target.replace("E#", "e");
         return target;
     }
+    
+    
     public class Music implements Comparable<Music> {
         int startTime;
         int playTime;
         String title;
         String code;
         
-        public Music(String start, String end, String title, String code, String m)  throws Exception {
+        public Music(String start, String end, String title, String code, String m) {
             int[] startT = Arrays.stream(start.split(":")).mapToInt(Integer::parseInt).toArray();
             this.startTime = startT[0]*60 + startT[1];
-            //if(end.equals("00:00")) end = "24:00";
             int[] endT = Arrays.stream(end.split(":")).mapToInt(Integer::parseInt).toArray();
             int endTime = endT[0]*60 + endT[1];
             this.playTime = endTime - this.startTime;
-            //if(m.contains("E#") || m.contains("B#")) throw new Exception();
             this.title = title;
             for(int i=0; i<=this.playTime; i++){
                 this.code += code.charAt(i%code.length())+"";
             }
-            //this.code = (code.length() >= m.length()) ? code.repeat(2) : code.repeat((m.length()/code.length())+1);
-        }
-        
-        public void check(){
-            if(this.playTime <= 1) return;
-            else if(this.code.length() >= this.playTime) this.code = this.code.substring(0, this.playTime-1);
-        }
-        
-        public boolean isfindCode(String target){
-            return code.contains(target);
         }
         
         @Override
