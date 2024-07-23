4e062d627efd1c1198e630acc934e49f1ee8fb9f
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-07-23 11:16:05 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 228 ms, Memory: 18380 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println((long)(Math.pow(a, 2)) + "\n" + 2);
+    }
+}
