f676235c3fc20a4e3ae5f0d334a858b32afb3af5
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-09-03 21:03:43 +0900

    [Bronze II] Title: 블랙잭, Time: 188 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,54 +1,32 @@
-import java.util.Scanner;
- 
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		int M = in.nextInt();
- 
-		int[] arr = new int[N];
- 
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
-		}
-		
-		int result = search(arr, N, M);
-		System.out.println(result);
-	}
- 
-	
-	// 탐색
-	static int search(int[] arr, int N, int M) {
-		int result = 0;
- 
-		// 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
-		for (int i = 0; i < N - 2; i++) {
- 
-			// 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
-			for (int j = i + 1; j < N - 1; j++) {
- 
-				// 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
-				for (int k = j + 1; k < N; k++) {
-					
-					// 3개 카드의 합 변수 temp
-					int temp = arr[i] + arr[j] + arr[k];
-					
-					// M과 세 카드의 합이 같다면 temp return 및 종료 
-					if (M == temp) {	
-						return temp;
-					}
-					
-					// 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신 
-					if(result < temp && temp < M) {
-						result = temp;
-					}
-				}
-			}
-		}
-		
-		// 모든 순회를 마치면 result 리턴 
-		return result;
-	}
+
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        int repeat = sc.nextInt();
+        int target = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<repeat; i++){
+            list.add(sc.nextInt());
+        }
+
+        int result = 0;
+        for(int i=0; i<repeat; i++){
+            int total = list.get(i);
+            for(int j=i+1; j<repeat; j++){
+                total += list.get(j);
+                for(int k=j+1; k<repeat; k++){
+                    total+= list.get(k);
+                    if(total <= target){
+                        if(result == 0) result = total;
+                        else result = result >= total ? result : total;
+                    }
+                    total -= list.get(k);
+                }
+                total -= list.get(j);
+            }
+        }
+        System.out.println(result);
+    }
 }
