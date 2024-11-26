cea17eaa9708266d74805fdfffd82d84d540600f
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-11-26 16:51:37 +0900

    [Silver IV] Title: 다음 소수, Time: 364 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -1,31 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int repeat = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < repeat; i++) {
+			long number = Long.parseLong(br.readLine());
+			if(number == 0 || number == 1) {
+				sb.append(2).append("\n");
+				continue;
+			}
+			long result = findNumber(number);
+			sb.append(result).append("\n");
+		}
+		System.out.println(sb.toString().trim());
+	}
 
-        for (long i = 0; i < n; i++) {
-            long a = sc.nextLong();
-            while (true) {
-                long count = 0;
-                for (long j = 2; j <= Math.sqrt(a); j++) {
-                    if (a % j == 0) {
-                        count++;
-                        break;
-                    }
-                }
-                if (count == 0) {
-                    if (a == 0 || a == 1) {
-                        a = 2;
-                    }
-                    System.out.println(a);
-                    break;
-                }
-                a++;
-            }
-        }
+	private static long findNumber(long number) {
+		long result = number;
+		while(true){
+			if(check(result)){
+				return result;
+			}
+			result++;
+		}
+	}
 
-        sc.close();
-    }
+	private static boolean check(long result) {
+		if(result == 2) return true;
+		double sqrt = Math.sqrt(result);
+		for(long i = 2; i<=sqrt; i++){
+			if(result%i == 0) return false;
+		}
+		return true;
+	}
 }
