b425d5db50682605f2162d3c5fdaf15d66a6f368
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-12-16 11:43:25 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,33 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			String S = br.readLine();
-			
-			//문자열의 각 자리의 값을 확인할때는 char형태로 바꿔서 확인하기 때문에 스택도 Character형으로 해준다.
-			Stack<Character> stack = new Stack<Character>();
-			
-			for(int j = 0; j < S.length(); j++) {
-				if(S.charAt(j) == '(') {
-					stack.push(S.charAt(j));
-				}else {
-					//비어있는지를 먼저 본다.
-					if(stack.empty()) {
-						stack.push(S.charAt(j));
-						break;
-					}else {
-						stack.pop();
-					}
-				}
-			}
-			if(stack.empty()) System.out.println("YES");
-			else System.out.println("NO");
-		}
-	}
 
+    public static void main(String[] args) throws Exception{
+        Stack<Character> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int repeat = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < repeat; i++){
+            stack.clear();
+            String s = br.readLine();
+            for(char ch : s.toCharArray()){
+                if(ch == '('){
+                    stack.push(ch);
+                    continue;
+                }
+                if(stack.isEmpty()){
+                    stack.push(ch);
+                    break;
+                }
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                sb.append("YES\n");
+            }
+            else sb.append("NO\n");
+        }
+        System.out.println(sb.toString().trim());
+    }
 }
