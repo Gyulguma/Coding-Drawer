47890769ae8d23b0b0f2539602e501775c310e20
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-12-16 11:47:15 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 24000 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,31 +1,22 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int K = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
-
-		for(int i=0; i<K; i++) {
-			int temp = Integer.parseInt(br.readLine());
-
-			if(temp == 0) {
-				stack.pop();
-			}
-			else {
-				stack.push(temp);
-			}
-
-		}
-
-		int sum = 0;
-		for(int num : stack) {
-			sum += num;
-		}
-
-		System.out.println(sum);
-
-	}
+    public static void main(String[] args) throws Exception{
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int repeat = Integer.parseInt(br.readLine());
+        for(int i = 0; i < repeat; i++){
+            int number = Integer.parseInt(br.readLine());
+            if(number == 0) stack.pop();
+            else stack.push(number);
+        }
+        int result = 0;
+        for(int number : stack){
+            result += number;
+        }
+        System.out.println(result);
+    }
 }
