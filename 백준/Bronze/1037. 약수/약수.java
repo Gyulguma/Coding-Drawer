76dc6f600dde0e1fe9c3cb9fc72fa547c8173986
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-12-17 19:00:26 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] numbers = new int[n];
         String[] inputs = br.readLine().split(" ");
+        int min = Integer.parseInt(inputs[0]);
+        int max = Integer.parseInt(inputs[0]);
         for(int i = 0; i<n; i++){
-            numbers[i] = Integer.parseInt(inputs[i]);
+            int number = Integer.parseInt(inputs[i]);
+            if(min > number) min = number;
+            if(max < number) max = number;
         }
-        Arrays.sort(numbers);
-        System.out.println(numbers[0]*numbers[n-1]);
+        System.out.println(min*max);
     }
 }
