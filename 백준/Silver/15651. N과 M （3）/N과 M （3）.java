5f08f1040c4be622df734fdc190f724132941c4c
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-12-26 15:43:55 +0900

    [Silver III] Title: N과 M (3), Time: 524 ms, Memory: 159056 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+        arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = i+1;
+        }
+
+        recursion(m, 0, "");
+        System.out.println(sb.toString().trim());
+    }
+
+    private static void recursion(final int cutLine, int length, String result) {
+        if(length == cutLine){
+            sb.append(result.trim()).append("\n");
+            return;
+        }
+        for(int i=0; i<arr.length; i++){
+            recursion(cutLine, length+1, result + arr[i] + " ");
+        }
+    }
+}
