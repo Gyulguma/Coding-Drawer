906f8309a649a774a1a59fc120f6ac9c42b585c1
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2025-02-17 20:24:44 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 24028 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,47 +1,26 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-	static int[] arr;		// 배열 
-	static Integer[] dp;		// 메모이제이션 할 dp
-	static int max;			// 최댓값 변수 
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		arr = new int[N];
-		dp = new Integer[N];
-		
-		for(int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
-		}
-		
-		/*
-		 * dp[0]은 첫 원소로 이전에 더이상 탐색할 것이 없기 때문에
-		 * arr[0] 자체 값이 되므로 arr[0]으로 초기화 해준다.
-		 * max또한 첫 번째 원소로 초기화 해준다.
-		 */
-		dp[0] = arr[0];
-		max = arr[0];
-		
-		// dp의 마지막 index는 N-1이므로 N-1부터 Top-Down 탐색 
-		recur(N - 1);
-		
-		System.out.println(max);
-	}
-	
-	static int recur(int N) {
-		
-		// 탐색하지 않은 인덱스라면
-		if(dp[N] == null) {
-			dp[N] = Math.max(recur(N - 1) + arr[N], arr[N]);
- 
-			// 해당 dp[N]과 max 중 큰 값으로 max 갱신 
-			max = Math.max(dp[N], max);
-		}
-		
-		return dp[N];
-	}
+    public static void main(String[] args) throws IOException {
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	int[] numbers = new int[n+1];
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	for(int j=1; j<=n; j++) {
+    		numbers[j] = Integer.parseInt(st.nextToken());
+    	}
+    	int[] dp = new int[n+1];
+    	int max = numbers[1];
+    	dp[1] = numbers[1];
+    	for(int i=2; i<=n; i++) {
+    		dp[i] = Math.max(dp[i-1]+numbers[i], numbers[i]);
+    		max = Math.max(max, dp[i]);
+    	}
+    	System.out.println(max);
+    }
+    
 }
