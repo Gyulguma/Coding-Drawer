adbe1e5f3f569deec6536b6d8a1ff63d0ad0833f
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2025-04-20 00:11:27 +0900

    [Silver III] Title: N과 M (7), Time: 644 ms, Memory: 219552 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static int[] num;
+	private static StringBuilder sb = new StringBuilder();
+	private static int n,m;
+	private static boolean[] visited;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+        	num[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(num);
+        visited = new boolean[n];
+        dfs(0, "");
+        System.out.println(sb.toString().trim());
+        
+    }
+    
+    private static void dfs(int dept, String current) {
+    	if(dept == m) {
+    		sb.append(current).append("\n");
+    		return;
+    	}
+    	
+    	for(int i=0; i<n; i++) {
+    		dfs(dept+1, current+num[i]+" ");
+    	}
+    }
+}
