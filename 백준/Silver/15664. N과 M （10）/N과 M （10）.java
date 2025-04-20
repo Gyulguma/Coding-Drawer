3a8f786e5d0eb4257e9475690a97f64bfcadf93f
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2025-04-20 17:04:09 +0900

    [Silver II] Title: N과 M (10), Time: 128 ms, Memory: 16676 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static int[] num;
+	private static StringBuilder sb = new StringBuilder();
+	private static int n,m;
+	private static boolean[] visited;
+	private static Set<String> set = new HashSet<>();
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+        	num[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(num);
+        visited = new boolean[n];
+        dfs(0, "", 0);
+        
+        System.out.println(sb.toString().trim());
+        
+    }
+    
+    private static void dfs(int dept, String current, int start) {
+    	if(dept == m) {
+    		String result = current.trim();
+    		if(!set.contains(result)) {
+    			sb.append(result).append("\n");
+    			set.add(result);
+    		}
+    		return;
+    	}
+    	
+    	for(int i=start; i<n; i++) {
+    		String next = current+num[i]+" ";
+    		dfs(dept+1, next, i+1);
+    	}
+    }
+}
