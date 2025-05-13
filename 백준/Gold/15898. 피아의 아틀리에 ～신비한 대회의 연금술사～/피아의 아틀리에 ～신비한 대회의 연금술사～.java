ebf2b841d82177ca7fc518eeb5c5985d374b115a
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2025-05-13 19:55:06 +0900

    [Gold I] Title: 피아의 아틀리에 ~신비한 대회의 연금술사~, Time: 1500 ms, Memory: 25360 KB -BaekjoonHub

---
+++ 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
@@ -0,0 +1,204 @@
+import java.io.*;
+import java.util.*;
+
+
+/*
+ * 재료(4x4)
+ * 	- 효능: -9~9
+ * 	- 원소: R/B/G/Y/W
+ * 
+ * 가마(5x5)
+ * 	- 품질: 초기 0 -(재료)-> 품질+효능(0~9)
+ * 	- 원소: 초기 W -(재료)-> 재료원소:W => 그대로 / 그 외 재료 원소 색으로.
+ * 
+ * 재료 3개 고른 뒤 순서를 정해 하나씩 가마에 넣음(90도씩 회전가능).
+ * 
+ * 빨강, 파랑, 초록, 노랑으로 칠해진 부분의 품질 합을 각각 R, B, G, Y
+ * 폭탄의 품질 = 7R + 5B + 3G + 2Y
+ * 
+ * 폭탄의 최대 품질 값
+ * */
+public class Main {
+	private static final int ORDER_SIZE = 3;
+	private static final int MATTER_SIZE = 4;
+	private static final int KILN_SIZE = 5;
+	
+	private static int n;
+	private static Matter[][] matters;
+	private static int[][] order;
+	private static int result;
+	private static Matter kiln;
+    private static int[][] position = {{0,0},{0,1},{1,0},{1,1}};
+    private static boolean[] visited;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        n = Integer.parseInt(br.readLine());
+        matters = new Matter[n][4];
+        for(int i=0; i<n; i++) {
+        	matters[i][0] = new Matter(MATTER_SIZE);
+        }
+        
+        //각 재료 초기화
+        for(int repeat=0; repeat<n; repeat++) {
+        	//재료
+        	Matter matter = matters[repeat][0];
+        	//효능 초기화
+        	for(int i=0; i<MATTER_SIZE; i++) {
+        		StringTokenizer st = new StringTokenizer(br.readLine());
+        		for(int j=0; j<MATTER_SIZE; j++) {
+        			matter.value[i][j] = Integer.parseInt(st.nextToken());
+        		}
+        	}
+        	//원소 초기화
+        	for(int i=0; i<MATTER_SIZE; i++) {
+        		StringTokenizer st = new StringTokenizer(br.readLine());
+        		for(int j=0; j<MATTER_SIZE; j++) {
+        			matter.element[i][j] = st.nextToken().charAt(0);
+        		}
+        	}
+        }
+        
+        //돌린거 저장
+        for(int i=0; i<n; i++) {
+        	for(int j=1; j<4; j++) {
+        		matters[i][j] = rotate(matters[i][j-1]);
+        	}
+        }
+        
+        //가마 초기화
+        kiln = new Matter(KILN_SIZE);
+        for(int i=0; i<KILN_SIZE; i++) {
+        	for(int j=0; j<KILN_SIZE; j++) {
+        		kiln.element[i][j] = 'W';
+        	}
+        }
+        
+        order = new int[ORDER_SIZE][3];
+        visited = new boolean[n];
+        result = 0;
+        dfs(0);
+        
+        System.out.println(result);
+    }
+    
+    private static Matter rotate(Matter matter) {
+    	Matter temp = new Matter(MATTER_SIZE);
+    	
+    	for(int i=0; i<MATTER_SIZE; i++) {
+    		for(int j=0; j<MATTER_SIZE; j++) {
+    			temp.value[i][j] = matter.value[MATTER_SIZE-1-j][i];
+    			temp.element[i][j] = matter.element[MATTER_SIZE-1-j][i];
+    		}
+    	}
+    	
+    	return temp;
+    }
+    
+    private static void dfs(int dept) {
+    	if(dept == ORDER_SIZE) {
+    		kiln.clear();
+    		for(int i=0; i<3; i++) {
+    			Matter matter = matters[order[i][0]][order[i][1]];
+    			putMatter(matter, position[order[i][2]][0], position[order[i][2]][1]);
+    		}
+    		
+    		int total = calcul();
+    		result = Math.max(result, total);
+    		return;
+    	}
+    	
+    	for(int i=0; i<n; i++) {
+    		if(visited[i]) continue;
+    		visited[i] = true;
+    		order[dept][0] = i;
+    		for(int repeat=0; repeat<4; repeat++) {
+    			order[dept][1] = repeat;
+    			for(int idx=0; idx<position.length; idx++) {
+    				order[dept][2] = idx;
+        			dfs(dept+1);
+        		}
+    		}
+    		visited[i] = false;
+    	}
+    }
+
+	private static void putMatter(Matter matter, int x, int y) {
+		for(int i=0; i<MATTER_SIZE; i++) {
+			for(int j=0; j<MATTER_SIZE; j++) {
+				int ni = i+x, nj = j+y;
+				//품질 변경
+				int value = kiln.value[ni][nj] + matter.value[i][j];
+				if(value < 0) value = 0;
+				if(value > 9) value = 9;
+				kiln.value[ni][nj] = value;
+				
+				//원소 변경
+				if(matter.element[i][j] == 'W') continue;
+				kiln.element[ni][nj] = matter.element[i][j];
+			}
+		}
+	}
+
+	private static int calcul() {
+		int r=0, b=0, g=0, y=0;
+		
+        for(int i=0; i<KILN_SIZE; i++) {
+        	for(int j=0; j<KILN_SIZE; j++) {
+        		int value = kiln.value[i][j];
+        		switch(kiln.element[i][j]) {
+        		case 'R': r += value; break;
+        		case 'B': b += value; break;
+        		case 'G': g += value; break;
+        		case 'Y': y += value; break;
+        		default: break;
+        		}
+        	}
+        }
+		
+		return 7*r + 5*b + 3*g + 2*y;
+	}
+
+	private static class Matter{
+    	int[][] value;
+    	char[][] element;
+    	
+    	public Matter(int size) {
+    		value = new int[size][size];
+    		element = new char[size][size];
+    	}
+
+    	public void clear() {
+    		for(int i=0; i<value.length; i++) {
+    			Arrays.fill(value[i], 0);
+    			Arrays.fill(element[i], 'W');
+    		}
+    	}
+    	
+		public Matter copy() {
+			Matter matter = new Matter(value.length);
+			for(int i=0; i<value.length; i++) {
+				for(int j=0; j<value.length; j++) {
+					matter.value[i][j] = value[i][j];
+					matter.element[i][j] = element[i][j];
+				}
+			}
+			return matter;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i<value.length; i++) {
+				sb.append("[");
+				for(int j=0; j<value.length-1; j++) {
+					sb.append(value[i][j]+""+element[i][j]+", ");
+				}
+				sb.append(value[i][value.length-1]+""+element[i][value.length-1]+"");
+				sb.append("]\n");
+			}
+			return sb.toString().trim();
+		}
+    }
+}
