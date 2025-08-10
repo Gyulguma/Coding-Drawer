d643128c4eea54245290a2f8377915a40c49232e
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2025-08-10 12:58:12 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22896 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,45 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	private static int n, m;
+	private static int[] numbers, results;
+	private static boolean[] visited;
+	private static StringBuilder sb = new StringBuilder();
 
-    private static boolean[] visited;
-    private static int[] numbers;
-    private static StringBuilder sb;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
-        int n = Integer.parseInt(inputs[0]);
-        int m = Integer.parseInt(inputs[1]);
-        sb = new StringBuilder();
-        visited = new boolean[n];
-        numbers = new int[n];
-
-        for(int i=1; i<=n; i++){
-            numbers[i-1] = i;
-        }
-        String result = "";
-        dfs(numbers, m, result, 0);
-        System.out.println(sb.toString().trim());
-    }
-
-    private static void dfs(final int[] numbers, final int m, String result, int length){
-        if(length >= m){
-            sb.append(result.toString().trim()).append("\n");
-            return;
-        }
-        for(int i=0; i<numbers.length; i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            dfs(numbers, m, result + numbers[i]+ " ", length+1);
-            visited[i] = false;
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[n];
+		for(int i=1; i<=n; i++) {
+			numbers[i-1] = i;
+		}
+		
+		results = new int[m];
+		visited = new boolean[n];
+		dfs(0, 0);
+		System.out.println(sb.toString());
+	}
+	
+	private static void dfs(int current, int count) {
+		if(count == m) {
+			for(int number : results) {
+				sb.append(number).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(visited[i]) continue;
+			visited[i] = true;
+			results[current] = numbers[i];
+			dfs(current+1, count+1);
+			visited[i] = false;
+		}
+	}
 }
