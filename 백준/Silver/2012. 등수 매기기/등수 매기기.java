b72411d8cb70257829925fa354a7860c5aa74391
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2026-03-21 15:53:19 +0900

    [Silver III] Title: 등수 매기기, Time: 772 ms, Memory: 57164 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[n];
+		for(int i=0; i<n; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(nums);
+		long answer = 0;
+		for(int i=1; i<=n; i++) {
+			answer += Math.abs((long)i-nums[i-1]);
+		}
+		
+		System.out.println(answer);
+	}
+}
+	
