6e08c210ecd55e6c1d70fc6a0e08904f9c664c55
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2026-04-04 23:51:26 +0900

    [Silver III] Title: N과 M (1), Time: 256 ms, Memory: 22980 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,16 +1,16 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
 	private static int n, m;
 	private static int[] numbers, results;
-	private static boolean[] visited;
 	private static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws IOException{
+	private static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
@@ -20,12 +20,13 @@ public class Main {
 		}
 		
 		results = new int[m];
-		visited = new boolean[n];
-		dfs(0, 0);
-		System.out.println(sb.toString());
+		visited = new boolean[n+1];
+		recur(0, 0);
+		
+		System.out.println(sb.toString().trim());
 	}
 	
-	private static void dfs(int current, int count) {
+	private static void recur(int curIdx, int count) {
 		if(count == m) {
 			for(int number : results) {
 				sb.append(number).append(" ");
@@ -34,11 +35,11 @@ public class Main {
 			return;
 		}
 		
-		for(int i=0; i<n; i++) {
+		for(int i=1; i<=n; i++) {
 			if(visited[i]) continue;
 			visited[i] = true;
-			results[current] = numbers[i];
-			dfs(current+1, count+1);
+			results[curIdx] = i;
+			recur(curIdx+1, count+1);
 			visited[i] = false;
 		}
 	}
