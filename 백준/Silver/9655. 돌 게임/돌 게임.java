805280562cd8f0c6112a688e5a8e4b75f4549fc6
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2026-04-06 19:52:41 +0900

    [Silver V] Title: 돌 게임, Time: 116 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -6,8 +6,30 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		if(n%2 == 0) System.out.println("CY");
-		else System.out.println("SK");
+		if(n==1) {
+			System.out.println("SK");
+			return;
+		}
+		if(n==2) {
+			System.out.println("CY");
+			return;
+		}
+		if(n==3) {
+			System.out.println("SK");
+			return;
+		}
+		
+		int[] dp = new int[n+1];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 1;
+		
+		for(int i=4; i<=n; i++) {
+			dp[i] = (dp[i-1]==1 && dp[i-3]==1) ? 2 : 1;
+		}
+		
+		System.out.println(dp[n]==1 ? "SK" : "CY");
 	}
 }
 	
