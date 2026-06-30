d3a6c4a66e2d8ac73081e3f293c5d5b4b0d539a0
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2026-06-30 15:45:35 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 1;
-        while(n > 1) {
-            if(n%2 != 0) answer++;
-            n /= 2;
-        }
-        
-        return answer;
+        return Integer.bitCount(n);
     }
 }
