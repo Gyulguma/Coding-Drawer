98bf892373f3d03de91b58b046e40360f6e79068
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2026-07-22 14:53:13 +0900

    [level 2] Title: [3차] n진수 게임, Time: 32.57 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,35 @@
+//0 2 4 6
+import java.util.*;
+
+class Solution {
+    private char[] num = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        List<Character> list = new ArrayList<>();
+        list.add('0');
+        int number = 1;
+        
+        while(list.size() < t*m) {
+            for(char c : change(number++, n).toCharArray()) list.add(c);
+        }
+        
+        for(int i=0; i<t; i++) {
+            answer += list.get(i*m + (p-1));
+        }
+        
+        return answer;
+    }
+    
+    private String change(int number, int target) {
+        String result = "";
+        
+        while(number > 0) {
+            int idx = number%target;
+            result = num[idx] + result;
+            number /= target;
+        }
+        
+        return result;
+    }
+}
