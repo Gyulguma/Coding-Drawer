967d807352cde3faca36edee692528d7d727389a
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2026-07-24 18:56:27 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.20 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,55 +1,45 @@
 import java.util.*;
 
 class Solution {
-    private int min = Integer.MAX_VALUE;
     public int solution(int n, int[][] wires) {
+        int answer = n;
         
-        int[][] graph = new int[n+1][n+1];
-        for(int i=0; i<wires.length; i++){
-            int[] wire = wires[i];
-            graph[wire[0]][wire[1]] = 1;
-            graph[wire[1]][wire[0]] = 1;
+        int[][] map = new int[n+1][n+1];
+        for(int[] wire : wires) {
+            map[wire[0]][wire[1]] = 1;
+            map[wire[1]][wire[0]] = 1;
         }
         
-        for(int i=0; i<wires.length; i++){
-            int[] wire = wires[i];
-            graph[wire[0]][wire[1]] = 0;
-            graph[wire[1]][wire[0]] = 0;
-            
-            counting(graph, n);
-            
-            graph[wire[0]][wire[1]] = 1;
-            graph[wire[1]][wire[0]] = 1;
+        for(int[] wire : wires) {
+            int count = bfs(map, wire, n);
+            int result = Math.abs(count - (n-count));
+            answer = Math.min(answer, result);
         }
-         
-        return min;
+        
+        return answer;
     }
     
-    private void counting(int[][] graph, int n){
-        int count1 = 0;
-        int count2 = 0;
-        
+    private int bfs(int[][] map, int[] wire, int n) {
+        Queue<Integer> q = new LinkedList<>();
         boolean[] visited = new boolean[n+1];
-        for(int i=1; i<=n; i++){
-            int count = 0;
-            if(visited[i]) continue;
-            Queue<Integer> q = new LinkedList<>();
-            q.offer(i);
-            visited[i] = true;
-            while(!q.isEmpty()){
-                int current = q.poll();
-                count++;
-                for(int j=1; j<=n; j++){
-                    if(visited[j] || graph[current][j] == 0) continue;
-                    q.offer(j);
-                    visited[j] = true;
-                }
+        q.offer(wire[0]);
+        visited[wire[0]] = true;
+        
+        int count = 0;
+        while(!q.isEmpty()) {
+            int current = q.poll();
+            count++;
+            
+            for(int i=1; i<=n; i++) {
+                if(map[current][i] == 0 || visited[i]) continue;
+                if(current == wire[0] && i == wire[1]) continue;
+                if(current == wire[1] && i == wire[0]) continue;
+                
+                q.offer(i);
+                visited[i] = true;
             }
-            if(count1 == 0) count1 = count;
-            else count2 = count;
         }
         
-        int dif = Math.abs(count1-count2);
-        min = Math.min(min, dif);
+        return count; 
     }
 }
