b9c4de0eb7189e85b5106136e132c4beafa5b19f
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2026-09-18 22:26:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 42.87 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,62 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
         
-        DoublePriorityQueue<Integer> dp = new DoublePriorityQueue<>();
-        for(String operation : operations){
-            String operator = operation.split(" ")[0];
-            int operand = Integer.parseInt(operation.split(" ")[1]);
+        for(String operation : operations) {
+            if(operation.equals("D 1")) {
+                minHeap.remove(maxHeap.poll());
+                continue;
+            }
+            if(operation.equals("D -1")) {
+                maxHeap.remove(minHeap.poll());
+                continue;
+            }
             
-            if(operator.equals("I")) {
-                dp.insert(operand);
-                continue;
-            }
-            if(operand == 1){
-                dp.removeMax();
-                continue;
-            }
-            if(operand == -1){
-                dp.removeMin();
-                continue;
-            }
+            int number = Integer.parseInt(operation.split(" ")[1]);
+            minHeap.offer(number);
+            maxHeap.offer(number);
         }
         
-        if(dp.isEmpty()) return new int[]{0,0};
-        return new int[]{dp.getMax(), dp.getMin()};
-    }
-    
-    private class DoublePriorityQueue<E extends Comparable<? super E>> {
-        private PriorityQueue<E> minHeap;
-        private PriorityQueue<E> maxHeap;
-        
-        public DoublePriorityQueue(){
-            this.minHeap = new PriorityQueue<>();
-            this.maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        }
-        
-        public void insert(E element){
-            this.minHeap.offer(element);
-            this.maxHeap.offer(element);
-        }
-        
-        public boolean isEmpty(){
-            return this.minHeap.isEmpty();
-        }
-        
-        public void removeMin(){
-            this.maxHeap.remove(this.minHeap.poll());
-        }
-        
-        public void removeMax(){
-            this.minHeap.remove(this.maxHeap.poll());
-        }
-        
-        public E getMin(){
-            return this.minHeap.peek();
-        }
-        
-        public E getMax(){
-            return this.maxHeap.peek();
-        }
+        if(minHeap.isEmpty()) return new int[]{0,0};
+        return new int[]{maxHeap.peek(), minHeap.peek()};
     }
 }
